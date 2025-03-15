ad62041fe3715042a61abd8281d32ff4d2b7504a
Sabbir Hosain <sabbirhosainbd59@gmail.com>
2025-03-16 00:46:11 +0600

    update

--- src/Components/Users/UserTable.jsx
+++ src/Components/Users/UserTable.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import DataTable from "react-data-table-component"
 import { BiEditAlt, BiTrash } from "react-icons/bi";
 import { BsEyeFill } from "react-icons/bs";
+import { FaCheck } from "react-icons/fa6";
 
 const UserTable = () => {
   const [error, setError] = useState(null)
@@ -9,45 +10,58 @@ const UserTable = () => {
 
   const columns = [
     {
-      name: "Serial No.",
+      name: "SL",
       selector: (row, index) => (index + 1),
+      width: "60px"
     },
     {
       name: "Name",
       selector: row => row.name,
+      width: "150px"
     },
     {
       name: "Email",
       selector: row => row.email,
+      width: "150px"
     },
     {
       name: "Phone Number",
       selector: row => row.phone,
+      width: "150px"
     },
     {
       name: "Phone Number",
       selector: row => row.phone,
+      width: "150px"
     },
     {
       name: "Phone Number",
       selector: row => row.phone,
+      width: "150px"
     },
     {
       name: "Phone Number",
       selector: row => row.phone,
+      width: "150px"
     },
     {
       name: "Phone Number",
       selector: row => row.phone,
+      width: "150px"
+    },
+    {
+      name: "Status",
+      selector: row => row === false ? <button style={{ backgroundColor: "red", padding: "5px 20px", color: "white", borderRadius: "0px" }}>Hold</button> : <button style={{ backgroundColor: "green", padding: "5px 20px", color: "white", borderRadius: "0px" }}>Active</button>
     },
     {
       name: "Action",
       cell: row => <div className="d-flex align-items-center gap-2">
+        <button className="btn btn-outline-primary rounded-0 btn-sm"><BsEyeFill /></button>
         <button data-bs-toggle="modal" data-bs-target="#exampleModal" className="btn btn-outline-success rounded-0 btn-sm"><BiEditAlt /></button>
         <button className="btn btn-outline-danger rounded-0 btn-sm"><BiTrash /></button>
-        {/* <button className="btn btn-outline-primary rounded-0 btn-sm"><BsEyeFill /></button> */}
-      </div>
-
+        <button className="btn btn-outline-dark rounded-0 btn-sm"><FaCheck /></button>
+      </div>,
+      width: "200px"
     }
   ];
 
